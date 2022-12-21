beea26f64968d6c3fea258e73d5d660f7ed36776
LANFOR MENA MONTALBÁN <lmena@crnova.com>
2022-12-20 23:30:05 -0600

    Se agrega angular/fire para comenzar a crear los servicios

--- MiniProyectoII/src/app/app.module.ts
+++ MiniProyectoII/src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MaterialModule } from './material.module';
+import { ReactiveFormsModule} from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -16,7 +20,10 @@ import { FooterComponent } from './components/footer/footer.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    ReactiveFormsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ MiniProyectoII/src/app/services/presupuesto.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {
+  Firestore,
+  addDoc,
+  collection,
+  collectionData,
+  deleteDoc,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  setDoc,
+  where,
+} from '@angular/fire/firestore';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PresupuestoService {
+
+  constructor() { }
+}
